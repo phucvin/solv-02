d187f01d51c1edefd4c4631d5fb081c222a86104
phucvin <phucvin@gmail.com>
2025-10-30 05:28:16 +0000

    add back index.html

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'Hello webpack 3';
-
-  return element;
-}
-
-document.body.appendChild(component());
-
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
