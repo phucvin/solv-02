e185fc573da9117ef8c0688208fee71fd00405bc
phucvin <phucvin@gmail.com>
2025-10-30 05:34:50 +0000

    re-add dist

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(async()=>{if("serviceWorker"in navigator)try{const e=await navigator.serviceWorker.register("/sw.js",{scope:"/"});e.installing?console.log("Service worker installing"):e.waiting?console.log("Service worker installed"):e.active&&console.log("Service worker active")}catch(e){console.error(`Registration failed with ${e}`)}})();
